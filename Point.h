af27facec89de7117950fe7f2b8b963fda3c916c
Francis R Scallatino <tup31915@temple.edu>
2022-09-12 13:51:03 -0400

    first commit

---
+++ Point.h
@@ -0,0 +1,19 @@
+#ifndef POINT_H
+#define POINT_H
+
+typedef struct Point{
+	double x;
+	double y;
+}Point;
+
+/**
+ * initilize and return a point struct value.
+ */
+Point Point_value(double x, double y);
+
+/**
+ * Print a representation of a Point to stdout
+ */
+void Pointer_print(Point *self);
+
+#endif
